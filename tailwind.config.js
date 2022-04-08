e6aed1cf2bb9f4a1595cb2845e8685fba4422217
nacen.dev <nacen.dev@gmail.com>
2022-04-08 19:15:24 +0800

    Add new custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"
-],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        babyBlue: "#DEEBF8",
+        lemony: "#FFFAD1",
+      },
+    },
   },
   plugins: [],
-}
+};
