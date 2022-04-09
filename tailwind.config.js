95bf4fa7408c7792eef87ce45ac26b60705bc83d
nacen.dev <nacen.dev@gmail.com>
2022-04-09 17:27:31 +0800

    Add textColor to extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         lemony: "#FFFAD1",
         grayBackground: "#F5F7FB",
         buttonColor: "#4D5B9E",
+        textColor: "#293264"
       },
       fontFamily: {
         Karla: ["Karla", "sans-serif"],
