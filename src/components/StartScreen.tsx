108dd6578c678ce0488fd2ce3c08121699563529
nacen.dev <nacen.dev@gmail.com>
2022-04-09 19:50:39 +0800

    Add start prop and remove height class

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import { Background } from "./Background";
 import { Button } from "./Button";
 
-type Props = {};
+type Props = {
+  start: () => void;
+};
 
-const StartScreen = (props: Props) => {
+const StartScreen = ({ start }: Props) => {
   return (
-    <Background className="h-full w-full flex items-center justify-center">
+    <Background className="w-full flex items-center justify-center">
       <div className="flex flex-col gap-5 items-center px-5">
-        <h1 className="text-4xl font-bold font-Karla text-textColor">Quizzical</h1>
-        <p className="text-lg text-textColor text-center">Test your knowledge with random trivia</p>
-        <Button className="w-full">Start quiz</Button>
+        <h1 className="text-4xl font-bold font-Karla text-textColor">
+          Quizzical
+        </h1>
+        <p className="text-lg text-textColor text-center">
+          Test your knowledge with random trivia
+        </p>
+        <Button onClick={start} className="w-full">
+          Start quiz
+        </Button>
       </div>
     </Background>
   );
