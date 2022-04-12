2e82e525ab12a32d396b4abdc04f3d6b3f6d9b8e
nacen.dev <nacen.dev@gmail.com>
2022-04-12 17:03:32 +0800

    Modify Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,16 @@ import React from "react";
 type Props = {
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 };
 
-const Button = ({ children, className }: Props) => {
+const Button = ({ children, className, onClick }: Props) => {
   return (
     <button
-      className={`px-4 py-3 text-xl text-white bg-buttonColor rounded-lg ${
+      className={`px-4 py-3 text-xl text-white bg-customViolet rounded-lg ${
         className ? className : ""
       }`}
+      onClick={onClick}
     >
       {children}
     </button>
